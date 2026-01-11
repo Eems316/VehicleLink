2f2e374c2e1a9179459d8d07fe0402796c1426df
Eems316 <alex.tomesek@gmail.com>
2026-01-11 03:26:16 -0500

    fix: About page margins and anchors/ids

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -52,8 +52,8 @@ export default function About() {
                 </InfoPanel >
             </div>
 
-            <InfoPanel className="mx-auto p-4 bg-textboxBackground text-textHeading max-w-5xl">
-                <h2 className="text-center mt-2 mb-4 ">Map of our yard</h2>
+            <InfoPanel className="mx-auto my-18 p-4 bg-textboxBackground text-textHeading max-w-5xl">
+                <h2 id="yard" className="text-center mt-2 mb-4 ">Map of our yard</h2>
                 <Image src={IMAGE_ROUTES.images + "lotMapId.svg"} 
                         height={550}
                         width={400} 
@@ -62,7 +62,7 @@ export default function About() {
             </InfoPanel>
 
             <InfoPanel className="mx-auto p-4 bg-textboxBackground text-textHeading max-w-5xl">
-                <h2 className="mt-2 mb-4 text-center">Get Directions!</h2>
+                <h2 id="map" className="mt-2 mb-4 text-center">Get Directions!</h2>
 
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5958.416061236006!2d-83.5514029!3d41.69444459999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883b80fe6d183959%3A0x38e4f1a93915198b!2s4096%20N%20Detroit%20Ave%2C%20Toledo%2C%20OH%2043612!5e0!3m2!1sen!2sus!4v1768106471445!5m2!1sen!2sus" 
                         allowFullScreen={true} 
